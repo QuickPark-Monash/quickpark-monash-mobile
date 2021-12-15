f34338937bd62e2c6f5eb96d9f507c5b20d66bfb
multipletwigs <starlaxone@gmail.com>
2021-12-15 10:46:28 +0800

    Poggers release

--- src/app/components/wallet/wallet.component.ts
+++ src/app/components/wallet/wallet.component.ts
@@ -16,7 +16,7 @@ export class WalletComponent implements OnInit {
       icon: "warning",
       text:"This page shows you your parking spendings and allows you to top up when you pay for parking tickets through our application.",
       footer:"It is currently under construction"
-    }).then(() => window.location.replace(""))
+    }).then(() => window.history.go(-1))
   }
 
 }
